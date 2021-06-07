6dccc70faa91ade9bcb99e3dbf6ad7f8bd8467dc
Admond Tamang <admondtamang@gmail.com>
2021-06-07 19:45:23 +0545

    [fix] login fix

--- pages/Login.js
+++ pages/Login.js
@@ -1,10 +1,10 @@
 import { Button } from "antd";
 import { signIn } from "next-auth/client";
 
-export default Login = () => {
+export default function Login() {
     return (
         <div>
             <Button onClick={() => signIn("github")}>github signin</Button>
         </div>
     );
-};
+}
